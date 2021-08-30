d8591d9646d448e576f9619b6b0b14b3f59a439c
Fabio Marangon <fabio_marangon@usmp.pe>
2021-08-29 20:34:56 -0500

    Agregando conexión a BD

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
 namespace lab1_marangon.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext:IdentityDbContext
     {
         
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<lab1_marangon.Models.VideoGame> DataVideoGame {get; set; }
+
     }
 }
