f54fbce504a77bf0d24e1a548086f14774718e67
Fabio Marangon <fabio_marangon@usmp.pe>
2021-08-29 21:00:44 -0500

    Agregando servicios

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using lab1_marangon.Data;
+
 namespace lab1_marangon
 {
     public class Startup
@@ -23,6 +27,9 @@ namespace lab1_marangon
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(
+                    Configuration.GetConnectionString("PostgressConnection")));            
             services.AddControllersWithViews();
         }
 
