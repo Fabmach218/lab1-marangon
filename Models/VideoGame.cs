813c3d8bae56e2d46b7af4ff6a7bea14e2a479c3
Fabio Marangon <fabio_marangon@usmp.pe>
2021-08-29 20:37:55 -0500

    Agregando nombre de tabla a entidad

--- Models/VideoGame.cs
+++ Models/VideoGame.cs
@@ -1,7 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace lab1_marangon.Models
 {
+    [Table("t_producto")]
     public class VideoGame
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id {get; set;}
         public string nombre {get; set;}
         public string categoria {get; set;}
